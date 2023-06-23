50190584a3af6cd80c5d9b39cf1d1bf427796380
Ayub <sir.kokabi@gmail.com>
2023-06-23 19:33:16 +0330

    add sentry

---
+++ src/hooks.server.js
@@ -0,0 +1,14 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://fc820bfca80447ba9730d1445570f774@o4505396522188800.ingest.sentry.io/4505409443856384',
+  tracesSampleRate: 1.0,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
