437460a978a9591f728bfdf13b63b9336cd08779
Ayub Kokabi <sir.kokabi@gmail.com>
2023-06-26 22:47:06 +0330

    Update hooks.server.js

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -5,6 +5,7 @@ import * as Sentry from '@sentry/sveltekit';
 Sentry.init({
   dsn: 'https://fc820bfca80447ba9730d1445570f774@o4505396522188800.ingest.sentry.io/4505409443856384',
   tracesSampleRate: 1.0,
+  debug: true,
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
